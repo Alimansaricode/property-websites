26b291f88678f1d3dc96539ac9e333ed448a039d
Alim Ansari <kkumar58647@gmail.com>
2026-03-18 19:26:33 +0530

    about update 2

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,11 +4,12 @@ import './About.css'
 function About() {
   return (
    <div className='contacta'>
+      <h1>Contact Us</h1>
       <form action="https://formspree.io/f/mwpypgqe" method='POST'>
         <input type="text" placeholder='Enter Your Name...' name='username' required />
         <input type="Email" placeholder='email' name='email' required />
         <textarea name="" placeholder='message' required></textarea>
-        <button>Submite</button>
+        <button>Submit</button>
       </form>
     </div>
   )
