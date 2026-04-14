0c65cf523b6711e6347bea1229150420bdf0ea36
Alim Ansari <kkumar58647@gmail.com>
2026-04-14 21:26:39 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Nav from './pages/nav'
+import Nav from './pages/Nav'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Buy from './pages/Buy'
